b1612f4eb60d73261c43bc2b46e1a7c15426428c
Michael Ilett <michael.ilett@metro.co.uk>
2025-06-05 13:28:33 +0100

    Testing and test plugins

---
+++ tests/phpunit/testdata/plugins/test-plugin-uninstall-constant-errors/load.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: Test Plugin Uninstall Constant without Errors for Plugin Check
+ * Plugin URI: https://github.com/WordPress/plugin-check
+ * Description: Some plugin description.
+ * Requires at least: 6.0
+ * Requires PHP: 5.6
+ * Version: 1.0.0
+ * Author: WordPress Performance Team
+ * Author URI: https://make.wordpress.org/performance/
+ * License: GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * Text Domain: test-plugin-uninstall-constant-without-errors
+ * Update URI: https://wordpress.org/plugins/example-plugin/
+ *
+ * @package test-plugin-uninstall-constant-without-errors
+ */
---
+++ tests/phpunit/tests/Checker/Checks/Plugin_Uninstall_Constant_Check.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Tests for the Plugin_Uninstall_Constant_Check class.
+ *
+ * @package plugin-check
+ */
+
+use WordPress\Plugin_Check\Checker\Check_Context;
+use WordPress\Plugin_Check\Checker\Check_Result;
+use WordPress\Plugin_Check\Checker\Checks\Plugin_Repo\Plugin_Uninstall_Constant_Check;
+
+class Plugin_Uninstall_Constant_Check_Tests extends WP_UnitTestCase {
+
+	public function test_run_with_plugin_uninstall_errors( ) {
+		$test_file = 'test-plugin-i18n-usage-without-errors/load.php';
+		$check_context = new Check_Context( UNIT_TESTS_PLUGIN_DIR . $test_file );
+		$check_result  = new Check_Result( $check_context );
+
+		$check = new Plugin_Uninstall_Constant_Check();
+		$check->run( $check_result );
+
+
+		$errors = $check_result->get_errors();
+
+		$this->assertNotEmpty( $errors );
+		$this->assertArrayHasKey( 'test-plugin-i18n-usage-without-errors/load.php', $errors );
+		$this->assertSame( 1, $check_result->get_error_count() );
+
+		$this->assertTrue( isset( $errors[ $test_file ][0][0][0] ) );
+		$this->assertSame( '', $errors[ $test_file ][0][0][0]['code'] );
+
+	}
+
+	public function test_run_without_any_errors() {
+		// Test plugin without any plugin updater.
+		$check_context = new Check_Context( UNIT_TESTS_PLUGIN_DIR . 'test-plugin-i18n-usage-without-errors/load.php' );
+		$check_result  = new Check_Result( $check_context );
+
+		$check = new Plugin_Uninstall_Constant_Check();
+		$check->run( $check_result );
+
+		$errors   = $check_result->get_errors();
+
+		$this->assertEmpty( $errors );
+
+		$this->assertEquals( 0, $check_result->get_error_count() );
+	}
+}
