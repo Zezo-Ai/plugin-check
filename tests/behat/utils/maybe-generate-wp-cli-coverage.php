a01bac3f7dbdae5f9b70bd8fee77a57ef2131c00
Nilambar Sharma <nilambar@outlook.com>
2025-09-04 11:01:30 +0545

    Update coverage setup

--- tests/behat/utils/maybe-generate-wp-cli-coverage.php
+++ tests/behat/utils/maybe-generate-wp-cli-coverage.php
@@ -2,41 +2,87 @@
 
 use SebastianBergmann\CodeCoverage\CodeCoverage;
 use SebastianBergmann\CodeCoverage\Driver\Selector;
+use SebastianBergmann\CodeCoverage\Driver\Xdebug;
 use SebastianBergmann\CodeCoverage\Filter;
-use SebastianBergmann\CodeCoverage\Report\Clover;
+use SebastianBergmann\CodeCoverage\Report\PHP as PHPReport;
 
 $root_folder = realpath( dirname( __DIR__, 3 ) );
+
+$feature   = getenv( 'BEHAT_FEATURE_TITLE' );
+$scenario  = getenv( 'BEHAT_SCENARIO_TITLE' );
+$step_line = (int) getenv( 'BEHAT_STEP_LINE' );
+$name      = "{$feature} - {$scenario} - {$step_line}";
+
+if ( empty( $feature ) || empty( $scenario ) ) {
+	return;
+}
 
 if ( ! class_exists( 'SebastianBergmann\CodeCoverage\Filter' ) ) {
 	require "{$root_folder}/vendor/autoload.php";
 }
 
-$filter = new Filter();
-$filter->includeDirectory( "{$root_folder}/includes" );
-$filter->includeFiles( array( "{$root_folder}/plugin.php" ) );
+$filtered_items = new CallbackFilterIterator(
+	new DirectoryIterator( $root_folder ),
+	function ( $file ) {
+		if ( $file->isDir() && in_array( $file->getFilename(), [ 'php', 'src' ], true ) ) {
+			return true;
+		}
 
-$coverage = new CodeCoverage(
-	( new Selector() )->forLineCoverage( $filter ),
-	$filter
+		if ( $file->isFile() && false !== strpos( $file->getFilename(), '-command.php' ) ) {
+			return true;
+		}
+
+		return false;
+	}
 );
 
-$feature  = getenv( 'BEHAT_FEATURE_TITLE' );
-$scenario = getenv( 'BEHAT_SCENARIO_TITLE' );
-$name     = "{$feature} - {$scenario}";
+$files = [];
+
+foreach ( $filtered_items as $item ) {
+	if ( $item->isDir() ) {
+		foreach (
+			new RecursiveIteratorIterator(
+				new RecursiveDirectoryIterator( $item->getPathname(), RecursiveDirectoryIterator::SKIP_DOTS )
+			) as $file
+		) {
+			if ( $file->isFile() && $file->getExtension() === 'php' ) {
+				$files[] = $file->getPathname();
+			}
+		}
+	} else {
+		$files[] = $item->getPathname();
+	}
+}
+
+$filter = new Filter();
+
+if ( method_exists( $filter, 'includeFiles' ) ) {
+	$filter->includeFiles( $files );
+} else {
+	$filter->addFilesToWhitelist( $files );
+}
+
+$coverage = new CodeCoverage(
+	class_exists( Selector::class ) ? ( new Selector() )->forLineCoverage( $filter ) : ( new Xdebug() ),
+	$filter
+);
 
 $coverage->start( $name );
 
 register_shutdown_function(
-	static function () use ( $coverage, $feature, $scenario, $name ) {
+	static function () use ( $coverage, $feature, $scenario, $step_line, $name, $root_folder ) {
 		$coverage->stop();
-
-		$project_dir = getenv( 'BEHAT_PROJECT_DIR' );
 
 		$feature_suffix  = preg_replace( '/[^a-z0-9]+/', '-', strtolower( $feature ) );
 		$scenario_suffix = preg_replace( '/[^a-z0-9]+/', '-', strtolower( $scenario ) );
-		$filename        = "clover-behat/{$feature_suffix}-{$scenario_suffix}.xml";
-		$destination     = "{$project_dir}/build/logs/{$filename}";
+		$db_type         = strtolower( getenv( 'WP_CLI_TEST_DBTYPE' ) );
+		$destination     = "$root_folder/build/logs/$feature_suffix-$scenario_suffix-$step_line-$db_type.cov";
 
-		( new Clover() )->process( $coverage, $destination, $name );
+		$dir = dirname( $destination );
+		if ( ! file_exists( $dir ) ) {
+			mkdir( $dir, 0777, true );
+		}
+
+		( new PHPReport() )->process( $coverage, $destination );
 	}
 );
